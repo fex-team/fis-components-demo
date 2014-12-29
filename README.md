53f73414373384c2d4ab58a3ab4fecc1be9a34d3
liaoxuezhi <liaoxuezhi@baidu.com>
2014-12-29 15:33:50 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 fis-components-demo
 ===================
 
-Fis components demo
+FIS 组件使用 demo.
+
+安装插件
+
+```bash
+npm install fis-parser-sass -g
+npm install fis-postprocessor-amd -g
+npm install fis-postpackager-autoload -g
+npm install fis-packager-depscombine -g
+```
--- fis-conf.js
+++ fis-conf.js
@@ -15,6 +15,13 @@ fis.config.set('roadmap.path', [
         useOptimizer: false
     },
 
+    // 不是 isMode 不会在 map.json 里面。
+    // 依靠它来读取 html 文件的依赖。
+    {
+        reg: '**.html',
+        isMod: true
+    },
+
     // 设置  libs 目录下面的文件为 `isMod`，这样 js 文件会自动包装成 amd 模块。
     {
         reg: /^\/libs\/(.*)$/i,
@@ -22,11 +29,24 @@ fis.config.set('roadmap.path', [
     }
 ].concat(fis.config.get('roadmap.path', [])));
 
+// 开起 autuload, 好处是，依赖自动加载。
+fis.config.set('modules.postpackager', 'autoload');
+fis.config.set('settings.postpackager.autoload.type', 'requirejs');
+
+fis.config.set('modules.parser.scss', 'sass');
+fis.config.set('modules.parser.sass', 'sass');
+fis.config.set('roadmap.ext.scss', 'css');
+fis.config.set('roadmap.ext.sass', 'css');
+
 fis.config.set('settings.parser.sass.include_paths', [
     path.join(__dirname, 'components/compass-mixins'),
     path.join(__dirname, 'scss')
 ]);
 
-// 开起 autuload, 好处是，依赖自动加载。
-fis.config.set('modules.postpackager', 'autoload');
-fis.config.set('settings.postpackager.autoload.type', 'requirejs');
+
+// 使用 depscombine 是因为，在配置 pack 的时候，命中的文件其依赖也会打包进来。
+fis.config.set('modules.packager', 'depscombine');
+fis.config.set('pack', {
+    'pkg/index.css': 'index.html',
+    'pkg/index.js': 'index.html'
+});
